7f38027d0b56f9e65f7d9d23f4e89b464b67b097
Danielle Balogh <daniellegb@protonmail.com>
2021-04-04 14:26:57 -0300

    first commit

---
+++ backend/src/main/java/com/devsuperior/trabalho_cliente_ds01/TrabalhoClienteDs01Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.trabalho_cliente_ds01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoClienteDs01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoClienteDs01Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/trabalho_cliente_ds01/TrabalhoClienteDs01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.trabalho_cliente_ds01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoClienteDs01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
